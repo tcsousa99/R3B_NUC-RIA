b0f30d77ab414e4f2e7e7159c7a8c79dfa035302
tcsousa99 <tcsousa99@gmail.com>
2021-06-29 16:02:05 +0000

    added the directory /fi4t fiber, a copy of /fi4 that I'll try and simulate to in the future understand the syntax of R3BRoot and be able to construct something from ctn

--- build/R3BRootVersion.h
+++ build/R3BRootVersion.h
@@ -1,5 +1,5 @@
 #ifndef R3BROOT_VERSION
-#define R3BROOT_VERSION may21-5-ga8be4d9c
-#define R3BROOT_GIT_DATE "Mon Jun 28 10:47:11 2021 +0000"
+#define R3BROOT_VERSION 
+#define R3BROOT_GIT_DATE "Mon Jun 28 14:30:41 2021 +0000"
 #endif
 
